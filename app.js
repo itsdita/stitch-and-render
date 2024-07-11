c0b6d06491684e57c73f6231fbb417e613b0d054
itsdita <dita.varna@gmail.com>
2024-07-11 14:13:34 +0900

    Updates

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.render('shared/landing-page', { title: 'Become PRO in CLO!' });
+});
+
+app.listen(3000);
