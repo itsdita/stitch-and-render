76bbbad59c391bd3695b57110180ac63ffb9e1ab
itsdita <dita.varna@gmail.com>
2024-07-15 14:57:16 +0900

    Added data fhalshing for signup

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -8,9 +8,24 @@ const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
-
 router.get("/join", (req, res) => {
-  res.render("shared/join", { title: "Signup!" });
+  let sessionInputData = req.session.inputData;
+
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      username: "",
+      email: "",
+      confirmEmail: "",
+      password: "",
+      confirmPassword: "",
+    };
+  }
+  
+  //flashing - clear data stored in session, so input doesn't show up all the time
+  req.session.inputData = null;
+
+  res.render("shared/join", { inputData: sessionInputData });
 });
 
 router.post("/join", async function (req, res) {
@@ -32,15 +47,29 @@ router.post("/join", async function (req, res) {
     password !== confirmPassword ||
     !email.includes("@")
   ) {
-    console.log("User input contains invalid data!");
-    return res.redirect("/join");
+    //temporarily storing signup data using session
+    req.session.inputData = {
+      hasError: true,
+      message: "Invalid input - please check your data.",
+      username: username,
+      email: email,
+      confirmEmail: confirmEmail,
+      password: password,
+      confirmPassword: confirmPassword,
+    };
+
+    req.session.save(function () {
+      res.redirect("/join");
+    });
+    return; //have to add this return so that the code below doesn't execute
+    //in case there is invalid input. If code below executes, server will crash.
   }
 
   const existingUserCheck = await db
     .getDb()
     .collection("users")
     .findOne({ email: email });
-  
+
   if (existingUserCheck) {
     console.log("User exists already!");
     return res.redirect("/join");
@@ -91,39 +120,41 @@ router.post("/login", async function (req, res) {
   }
 
   //storing authentication data in session
-  req.session.user = {id: existingUser._id.toString(), email: existingUser.email};
+  req.session.user = {
+    id: existingUser._id.toString(),
+    email: existingUser.email,
+  };
   req.session.isAuthenticated = true;
-  req.session.save(function(){
+  req.session.save(function () {
     res.redirect("/mypage"); //will execute only when session data is saved
   });
-  
 });
 
 // Protected route
 router.get("/mypage", (req, res) => {
   if (!req.session.isAuthenticated) {
-    return res.status(401).redirect('/login'); // Redirect to login if not authenticated
+    return res.status(401).redirect("/login"); // Redirect to login if not authenticated
   }
-  res.render("client/mypage", { 
+  res.render("client/mypage", {
     title: "WelcomeBack!",
-    isAuthenticated: req.session.isAuthenticated // Pass authentication status to view
+    isAuthenticated: req.session.isAuthenticated, // Pass authentication status to view
   });
 });
 
 router.get("/admin", function (req, res) {
   if (!req.session.isAuthenticated) {
-    return res.status(401).render('401'); // Typical status code for denied access
+    return res.status(401).render("401"); // Typical status code for denied access
   }
-  res.render("admin/admin-page", { 
+  res.render("admin/admin-page", {
     title: "Admin area!",
-    isAuthenticated: req.session.isAuthenticated // Pass authentication status to view
+    isAuthenticated: req.session.isAuthenticated, // Pass authentication status to view
   });
 });
 
-  router.post("/logout", (req, res) => {
-    req.session.user = null; //clearing user data in session
-    req.session.isAuthenticated = false;
-    res.redirect("/");
-  });
+router.post("/logout", (req, res) => {
+  req.session.user = null; //clearing user data in session
+  req.session.isAuthenticated = false;
+  res.redirect("/");
+});
 
 module.exports = router;
