ed896fc3f81d963e0e0ebb87ce45a1c0395008eb
itsdita <dita.varna@gmail.com>
2024-07-23 12:41:52 +0900

    basic blog feature created

---
+++ models/post.js
@@ -0,0 +1,17 @@
+const db = require("../data/database");//access to database
+
+class Post{
+    constructor(title, content,id){
+        this.title = title;
+        this.content = content;
+        this.id = id; //may be undefined if no id
+    }
+    async save(){
+            await db.getDb().collection('posts').insertOne({
+            title: this.title,
+            content:this.content,
+        });
+    }
+}
+
+module.exports = Post;
--- routes/public-routes.js
+++ routes/public-routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const router = express.Router();
+const Post = require("../models/post");
+const db = require("../data/database");//access to database
 
 mongoose.connect("mongodb://localhost:27017/stitchAndRender");
 
@@ -13,13 +15,44 @@ const contactSchema = new mongoose.Schema({
   message: String,
 });
 
-//Contact is the model name, and Mongoose will use this model to interact with the 
+//Contact is the model name, and Mongoose will use this model to interact with the
 //contacts collection in the MongoDB database
 //if want custom collection name, have to add it, after contactSchema
 const Contact = mongoose.model("Contact", contactSchema, "contactFormData");
 
 router.get("/", (req, res) => {
   res.render("shared/landing-page", { title: "Become PRO in CLO!" });
+});
+
+router.get("/blog", async function (req, res) {
+  const posts = await db.getDb().collection("posts").find().toArray();//fetching posts data
+  res.render("shared/blog", { title: "Some Deep 3D Thoughts.", posts: posts });//in {} data passed to template
+});
+
+router.post("/admin", async function (req, res) {
+  const enteredTitle = req.body.title;
+  const enteredContent = req.body.content;
+
+  if (
+    !enteredTitle ||
+    !enteredContent ||
+    enteredTitle.trim() === "" ||
+    enteredContent.trim() === ""
+  ) {
+    req.session.inputData = {
+      hasError: true,
+      message: "Invalid input - please check your data.",
+      title: enteredTitle,
+      content: enteredContent,
+    };
+    res.redirect("/admin");
+    return;
+  }
+
+  const newPost = new Post(enteredTitle, enteredContent);
+  await newPost.save();
+
+  res.redirect("/admin");
 });
 
 router.get("/contact", (req, res) => {
