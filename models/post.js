95e164f97fe8e7add85d1cf719218144f2f4db19
itsdita <dita.varna@gmail.com>
2024-07-27 14:02:43 +0900

    date and time are being recorded when making a new post

--- models/post.js
+++ models/post.js
@@ -1,17 +1,18 @@
-const db = require("../data/database");//access to database
+const db = require("../data/database"); //access to database
 
-class Post{
-    constructor(title, content,id){
-        this.title = title;
-        this.content = content;
-        this.id = id; //may be undefined if no id
-    }
-    async save(){
-            await db.getDb().collection('posts').insertOne({
-            title: this.title,
-            content:this.content,
-        });
-    }
+class Post {
+  constructor(title, content, id) {
+    this.title = title;
+    this.content = content;
+    this.id = id; //may be undefined if no id
+  }
+  async save() {
+    await db.getDb().collection("posts").insertOne({
+      title: this.title,
+      content: this.content,
+      date: new Date(), // recording date and time
+    });
+  }
 }
 
 module.exports = Post;
