1dc1960d87bfeccb60a0da791cd6faf17a327d82
itsdita <dita.varna@gmail.com>
2024-08-12 09:52:05 +0900

    added 404 error handler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,7 +1,12 @@
 //server error middleware
 function handleErrors(error, req, res, next) {
-    console.log(error);
-    res.status(500).render("shared/500");
+  console.log(error);
+
+  if (error.code === 404) {
+    return res.status(404).render("./404");
+  }
+
+  res.status(500).render("./500");
 }
 
 module.exports = handleErrors;
